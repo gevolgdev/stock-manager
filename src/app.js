9059913e4d03c71699930251f3e7844624d12bc8
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-18 11:02:36 -0300

    Adding Products

--- src/app.js
+++ src/app.js
@@ -1,5 +1,43 @@
-// Imports
+// Stock
 const stock = document.getElementById('stock-body');
-// const 
+// Add Products
+const inputName = document.getElementById('name');
+const inputPrice = document.getElementById('price');
+const inputQtd = document.getElementById('quantity');
+const addButton = document.getElementById('add-button');
 
 // -------------------------------------------------
+
+const stockProducts = [];
+const product = {};
+
+const renderProducts = (arr) => {
+  stock.innerHTML = '';
+  arr.forEach(item => {
+    const tr = document.createElement('tr');
+    for(let value in item) {
+      const td = document.createElement('td');
+      td.textContent = item[value];
+      tr.appendChild(td);
+    }
+    stock.appendChild(tr);
+  });
+};
+
+const isAddingProduct = e => {
+  const { id, value } = e.target;
+  Object.assign(product, {[id]: value});
+};
+const inputs = [inputName, inputPrice, inputQtd];
+inputs.forEach(item => {
+  item.addEventListener('change', isAddingProduct);
+});
+
+const handleAddProduct = e => {
+  e.preventDefault();
+  let newProduct = {...product};
+  stockProducts.push(newProduct);
+  renderProducts(stockProducts);
+};
+
+addButton.addEventListener('click', handleAddProduct);
