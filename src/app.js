d7878113ded3906c72dd4f053489f4249a17238e
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-18 15:00:37 -0300

    sellProduct ok

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ const inputName = document.getElementById('name');
 const inputPrice = document.getElementById('price');
 const inputQtd = document.getElementById('quantity');
 const addButton = document.getElementById('add-button');
-
+// Sell Products
+const inputSellName = document.getElementById('nameSell');
+const inputSellQtd = document.getElementById('quantitySell');
+const sellButton = document.getElementById('sell-button');
 // -------------------------------------------------
 
 const stockProducts = [];
 const product = {};
 
+// Render Products
 const renderProducts = (arr) => {
   stock.innerHTML = '';
   arr.forEach(item => {
@@ -24,6 +28,7 @@ const renderProducts = (arr) => {
   });
 };
 
+// Adding Products
 const isAddingProduct = e => {
   const { id, value } = e.target;
   Object.assign(product, {[id]: value});
@@ -38,6 +43,44 @@ const handleAddProduct = e => {
   let newProduct = {...product};
   stockProducts.push(newProduct);
   renderProducts(stockProducts);
+
+  inputs.forEach(item => item.value = '');
+}; 
+addButton.addEventListener('click', handleAddProduct);
+
+// Selling Products
+const soldProduct = {};
+
+const sellProduct = (product) => {
+  for (let i = 0; i < stockProducts.length; i++) {
+    if(product.nameSell === stockProducts[i].name){
+      if(product.quantitySell > stockProducts[i].quantity) {
+        console.log('Não há estoque suficiente!');
+        break;
+      };
+      const soldQuantity = stockProducts[i].quantity - product.quantitySell;
+      stockProducts[i].quantity = soldQuantity;
+      stockProducts[i].quantity === 0 && delete stockProducts[i]
+      break;
+    }
+  }
+  renderProducts(stockProducts);
 };
 
-addButton.addEventListener('click', handleAddProduct);
+const isSellingProduct = (e) => {
+  const { id, value } = e.target;
+  Object.assign(soldProduct, {[id]: value});
+};
+
+const sellingInputs = [inputSellName, inputSellQtd];
+sellingInputs.forEach(item => {
+  item.addEventListener('change', isSellingProduct);
+});
+
+const handleSellProduct = e => {
+  e.preventDefault();
+  let newSoldProduct = {...soldProduct};
+  sellProduct(newSoldProduct);
+  sellingInputs.forEach(item => item.value = '');
+};
+sellButton.addEventListener('click', handleSellProduct);
